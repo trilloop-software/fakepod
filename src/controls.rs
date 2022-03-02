b0adb4b1cac1128efc2718e4f47d07afad8de5e7
jchou25101 <jchou22@uwo.ca>
2022-03-01 22:19:28 -0500

    Handling Device specific commands
    -Added handlers for the device specific commands (the same commands that are part of the discovery packet)

--- src/controls.rs
+++ src/controls.rs
@@ -19,7 +19,7 @@ impl ControlsSvc {
             s!["Placeholder Cmd B"],
             s!["Placeholder Cmd C"],
         ];
-        let placeholder_cmds =vec![2,3,4];
+        let placeholder_cmd_codes =vec![2,3,4];
 
         loop {
             tokio::select! {
@@ -29,17 +29,22 @@ impl ControlsSvc {
                     let mut packet = cmd.unwrap();
 
                     match packet.cmd_type {
+
+                        //0 is reserved for error packets
+                        0 =>{
+                            
+                        }
                         //handle request for retrieving available commands for device controls
                         //part of the discovery packet
                         1 =>{
                             //build and encode a payload containing:
                             //  -a Vec<String> of command names
                             //  -a Vec<u8> of commands
-                            let commands = placeholder_cmds.clone();
+                            let commands = placeholder_cmd_codes.clone();
                             let names = placeholder_cmd_names.clone();
                             let mut resp_payload = decode_payload(packet.payload);
                             resp_payload.command_names = names;
-                            resp_payload.commands = commands;
+                            resp_payload.command_codes = commands;
 
                             //modify the packet to:
                             // -have an updated id
@@ -49,7 +54,27 @@ impl ControlsSvc {
                             //send the packet back to pod_conn
                             self.tx.send(packet).await;
                         }
-                        _ => ()
+                        // device specific command
+                        2 =>{
+                            println!("Received device specific command");
+                            println!("Executing cmd. cmd_code = 2");
+                            println!("NOTE: This is a placeholder command");
+                        }
+                        // device specific command
+                        3 =>{
+                            println!("Received device specific command");
+                            println!("Executing cmd. cmd_code = 3");
+                            println!("NOTE: This is a placeholder command");
+                        }
+                        // device specific command
+                        4 =>{
+                            println!("Received device specific command");
+                            println!("Executing cmd. cmd_code = 4");
+                            println!("NOTE: This is a placeholder command");
+                        }
+                        _ => {
+                            println!("No matching command was found");
+                        }
                     }
                 }
             }
--- src/pod_packet_payload.rs
+++ src/pod_packet_payload.rs
@@ -6,19 +6,23 @@ use bincode::{serialize, deserialize};
 //template used in discovery packets
 pub struct PodPacketPayload {
 
+    pub target_id: String,
+    pub target_cmd_code: u8,
     pub field_names: Vec<String>,
     pub telemetry_data: Vec<u8>, //assume all data is of type u8 for now (maybe make this Vec<Vec<u8>> later?)
     pub command_names: Vec<String>,
-    pub commands: Vec<u8>,
+    pub command_codes: Vec<u8>,
 }
 
 impl PodPacketPayload {
     pub fn new() -> Self {
         Self {
+            target_id: s![""],
+            target_cmd_code: 0,
             field_names: Vec::new(),
             telemetry_data: Vec::new(), //assume all data is of type u8 for now (maybe make this Vec<byte array> later?)
             command_names: Vec::new(),
-            commands: Vec::new(),
+            command_codes: Vec::new(),
         }
     }
     
